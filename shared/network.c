bdddc7c8be5db1eb29610c97fdc3503d2e5ccd7b
Daniel McCarthy/AKA NibbleBits <dansblackberry2@hotmail.co.uk>
2021-12-03 23:44:18 +0000

    commit

--- shared/network.c
+++ shared/network.c
@@ -587,7 +587,7 @@ void giveme_network_load_ips()
         giveme_log("inet_aton() failed\n");
     }
 
-    giveme_network_ip_address_add(addr);
+    //giveme_network_ip_address_add(addr);
 
     if (inet_aton("67.205.184.222", &addr) == 0)
     {
