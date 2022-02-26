dd46b8a87dc6f2c45c922ea7de33e81780828f65
Daniel McCarthy/AKA NibbleBits <dansblackberry2@hotmail.co.uk>
2022-02-26 00:19:19 +0000

    Improved our UPNP client now it will be able to get the local IP from the interface if needed..

--- shared/upnp.c
+++ shared/upnp.c
@@ -1,13 +1,98 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/netdevice.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <miniupnpc/miniupnpc.h>
 #include <miniupnpc/miniwget.h>
 #include <miniupnpc/upnpcommands.h>
 #include <miniupnpc/portlistingparse.h>
 #include <miniupnpc/upnperrors.h>
 
-#include <stddef.h>
-#include <stdio.h>
+#include "log.h"
 
-#include <stddef.h>
+
+int interface_my_ip_find(char *addr_out)
+{
+    int s;
+    int res = 0;
+    struct ifconf ifconf;
+    struct ifreq ifr[50];
+    int ifs;
+    int i;
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0)
+    {
+        res = -1;
+        goto out;
+    }
+
+    ifconf.ifc_buf = (char *)ifr;
+    ifconf.ifc_len = sizeof ifr;
+
+    if (ioctl(s, SIOCGIFCONF, &ifconf) == -1)
+    {
+        res = -1;
+        goto out;
+    }
+
+    ifs = ifconf.ifc_len / sizeof(ifr[0]);
+    for (i = 0; i < ifs; i++)
+    {
+        char ip[INET_ADDRSTRLEN];
+        struct sockaddr_in *s_in = (struct sockaddr_in *)&ifr[i].ifr_addr;
+
+        if (!inet_ntop(AF_INET, &s_in->sin_addr, ip, sizeof(ip)))
+        {
+            res = -1;
+            goto out;
+        }
+
+        // Right now we ignore 0 and 1 starts to avoid ip such as
+        // 127.0.0.1 and 0.0.0.0 .. THis is not a clever way to resolve the local IP
+        // and there are better ways, but this is a fail safe in the event
+        // UPNP fails on us..
+        if (isdigit(ip[0]) && ip[0] != '0' && ip[0] != '1')
+        {
+            strncpy(addr_out, ip, sizeof(ip));
+            res = 0;
+            break;
+        }
+    }
+
+out:
+    if (s)
+    {
+        close(s);
+    }
+    return res;
+}
+
+int upnp_get_my_local_ip(char *lanaddr_out)
+{
+    if (!lanaddr_out)
+    {
+        return -1;
+    }
+
+    // Not NULL? Then its already been set.
+    if (lanaddr_out[0])
+    {
+        return 0;
+    }
+
+    // NULL bytes?? Then UPNP Local IP Lookup failed.. We are going to have to
+    // find it another way
+    return interface_my_ip_find(lanaddr_out);
+}
+
 /* Test function
  * 1 - get connection type
  * 2 - get extenal ip address
@@ -50,7 +135,6 @@ static int SetRedirectAndTest(struct UPNPUrls *urls,
                                    proto, remoteHost, leaseDuration, reservedPort);
         if (r == UPNPCOMMAND_SUCCESS)
             eport = reservedPort;
-
     }
     else
     {
@@ -78,14 +162,15 @@ static int SetRedirectAndTest(struct UPNPUrls *urls,
 
 int upnp_redirect(int localPort, int publicPort)
 {
-    int error = 0;
+    int res = 0;
     struct UPNPDev *devlist = NULL;
-    devlist = upnpDiscover(2000, "", "", UPNP_LOCAL_PORT_ANY, 0, 2, &error);
+    devlist = upnpDiscover(2000, "", "", UPNP_LOCAL_PORT_ANY, 0, 2, &res);
     if (!devlist)
     {
         // No UPNP devices were discovered, let us hope they are not behind a router
         // as they will struggle to use the network properly.
-        return -1;
+        res = -1;
+        goto out;
     }
 
     struct UPNPDev *device = NULL;
@@ -93,23 +178,33 @@ int upnp_redirect(int localPort, int publicPort)
     {
         struct UPNPUrls urls;
         struct IGDdatas data;
-        char lanaddr[64] = "unset"; /* my ip address on the LAN */
+        char lanaddr[64] = {};
 
         int i = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
         if (i < 0)
         {
             break;
         }
-     
+
+        // Let's set our LAN address based on UPNP response or alternatively
+        // interface lookup if the UPNP failed..
+        i = upnp_get_my_local_ip(lanaddr);
+        if (i < 0)
+        {
+            giveme_log("%s We still failed to find our LOCAL ip address.. UPNP port forward will not be possible\n", __FUNCTION__);
+            res = -1;
+            goto out;
+        }
+
         char port_l[6] = {0};
         char port_p[6] = {0};
         sprintf(port_l, "%i", localPort);
         sprintf(port_p, "%i", publicPort);
 
-        SetRedirectAndTest(&urls, &data, lanaddr, port_l, port_p, "tcp", "14400", "", "Rhythm Network", 1);
+        SetRedirectAndTest(&urls, &data, lanaddr, port_l, port_p, "tcp", "14400", "", "Rhythm Network", 0);
         FreeUPNPUrls(&urls);
     }
 
-    return 0;
+out:
+    return res;
 }
-
