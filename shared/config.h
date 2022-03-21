ab3f4ced084ae5042e0907b0006c5346f68cb76d
Daniel McCarthy/AKA NibbleBits <dansblackberry2@hotmail.co.uk>
2022-03-21 21:23:32 +0000

    changed timeout to 1 second for I/O operations

--- shared/config.h
+++ shared/config.h
@@ -61,8 +61,8 @@
 #define GIVEME_TCP_SERVER_MAX_CONNECTIONS 100
 // If we have no client accepted or message within 2 seconds we will drop the TCP connection
 #define GIVEME_NETWORK_TCP_CONNECT_TIMEOUT_SECONDS 2
-// 10 seconds waiting on a recv or send and we will timeout.
-#define GIVEME_NETWORK_TCP_IO_TIMEOUT_SECONDS 10
+// 1 seconds waiting on a recv or send and we will timeout.
+#define GIVEME_NETWORK_TCP_IO_TIMEOUT_SECONDS 1
 
 #define GIVEME_NETWORK_TCP_DATA_EXCHANGE_LISTEN_TIMEOUT 10
 
